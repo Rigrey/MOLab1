997a0f8381df12b8f0bc8d81b27fc086ae1642c1
Rigrey <ozhogin.m@gmail.com>
2024-10-20 11:41:19 +0000

    Done

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from simp_solv import execute_simplex
 def main():
     minimize = False
     c = [7, 7, 6]
-    A = [[2, 1, 1], [1, 2, 0], [0, 0.5, 4]]
+    A = [[2, 1, 1],
+         [1, 2, 0],
+         [0, 0.5, 4]]
     b = [8, 2, 6]
     f = 0
     print("Answer:", execute_simplex(c, A, b, f, minimize))
